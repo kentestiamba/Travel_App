410a96553a6a4f5280d2afcde7361e557eed0df5
Kem Paez <kemkylepaez@gmail.com>
2023-11-28 00:43:12 +0800

    Configured constants font assets

--- travelGo/src/screens/MainScreens/DashBoardScreen/index.js
+++ travelGo/src/screens/MainScreens/DashBoardScreen/index.js
@@ -4,9 +4,11 @@ import styles from './styles';
 
 export default function DashBoardScreen() {
 
-    return (
-        <View style={styles.container}>
-            <Text>This is DashBoardScreen</Text>
-        </View>
-    )
+	return (
+		<View style={styles.container}>
+			<Text style={styles.font400}>Test Font Style DASHBOARD!</Text>
+			<Text style={styles.font700}>Test Font Style DASHBOARD!</Text>
+			<Text style={styles.font900}>Test Font Style DASHBOARD!</Text>
+		</View>
+	)
 }
--- travelGo/src/screens/MainScreens/DashBoardScreen/styles.js
+++ travelGo/src/screens/MainScreens/DashBoardScreen/styles.js
@@ -1,12 +1,33 @@
 import { Dimensions, StyleSheet } from 'react-native';
+import constants from '../../../constants'
 const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
+        justifyContent: 'center',
         backgroundColor:"#fff",
-    }
+    },
+
+    font400: {
+        fontSize: 16,
+        color: '#000',
+        fontFamily: constants.fonts.workSans400
+    },
+
+    font700: {
+        fontSize: 20,
+        color: '#000',
+        fontFamily: constants.fonts.reemKufiInk700
+    },
+
+    font900: {
+        fontSize: 27,
+        color: '#000',
+        fontFamily: constants.fonts.roboto900
+    },
+
 });
 
 export default styles;
